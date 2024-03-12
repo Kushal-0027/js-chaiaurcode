bdbcd0a41037d6ec03b4c2917dcc11a47686b4ee
Kushalmee <121502307+Kushal-0027@users.noreply.github.com>
2024-03-12 06:15:25 +0000

    datatypes are done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // After writing this the javascript will be treated as a newer-version of JS
+
+// alert(3+3) // We are using NodeJS not a Browser
+
+let name = "Kushal" // string
+let age = 4 // variable
+let isLoggedIn = false // Boolean
+
+// this is the range which we get in memory address [2 (powerof)^ 53]
+// To get more we can also use bigint => This bigint specially used in Trading/Stock Market or in big website like reddit, facebook , instagram etc.
+// Basic datattype is String => Preferrable to use string in the ""
+
+// boolean is also a value which has either true or false
+// null => is an standalone value
