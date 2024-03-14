2bbd5fac14eb994b3c71eaf9d2cba64d7c63f1c0
Kushalmee <121502307+Kushal-0027@users.noreply.github.com>
2024-03-14 11:24:39 +0000

    datatype comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,18 @@
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 != 1);
+console.log(2 == 1);
+console.log(2 >= 1);
+
+console.log("2" > 1); // if you want to change datatypes just see that the datatypes of both should be same
+console.log("02" > 1);
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0) // Output of this will be true bcz the problem occured while converting it
+
+ console.log(undefined == 0); // No matter what but if you perform 'Undefined' with any of this [==,>=,>,<] it will return false everytime
+ console.log(undefined >= 0);
+ console.log(undefined > 0);
+
+ console.log("2" === 2); // It will check datatype also
